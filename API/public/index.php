800b0f6cd8e17d12f088ce04cccee9c4713dcbfb
ozzieozk <55143838+ozzieozk@users.noreply.github.com>
2021-03-22 16:39:05 +0100

    Cors probl

--- API/public/index.php
+++ API/public/index.php
@@ -1,8 +1,33 @@
 <?php
 
- require_once __DIR__ . '/../src/t.php' ;
- require_once __DIR__ . '/../src/vendor/autoload.php' ;
+require_once '../src/vendor/autoload.php' ;
 
- $app = new \Slim\App();
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
- test();
+
+$settings       = require_once '../config/settings.php' ;
+$dependencies   = require_once '../config/dependencies.php';
+$errors         = require_once '../config/errors.php';
+ 
+// $config = parse_ini_file($settings['settings']['dbfile']);
+// $db = new \Illuminate\Database\Capsule\Manager();
+// $db->addConnection($config);
+// $db->setAsGlobal();
+// $db->bootEloquent();
+
+$c = new \Slim\Container(array_merge($settings, $dependencies, $errors));
+$app = new \Slim\App($c);
+
+$app->add(ReunionouAPI\Middlewares\Cors::class . ':checkAndAddCorsHeaders');
+
+$app->options('/{routes:.+}', function(Request $rq, Response $rs, array $args) {
+    return $rs;
+});
+
+$app->get('/hello', function(Request $req, Response $resp, $args) {
+    $resp->getBody()->write('salut bastien t es un peu con sur les bord non ? spoiler va');
+    return $resp;
+});
+
+$app->run();
