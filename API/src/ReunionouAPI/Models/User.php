e5310b2e6f52201188b9d07400219dc95d1de743
Evann Gehin <evann.gehin@ezcorp.io>
2021-03-24 09:26:07 +0100

    Hide user token

--- API/src/ReunionouAPI/Models/User.php
+++ API/src/ReunionouAPI/Models/User.php
@@ -9,6 +9,6 @@ class User extends Model
     protected $table = 'users';
     protected $primaryKey = 'id';
     public $timestamps = false;
-    protected $hidden = ['password'];
+    protected $hidden = ['password', 'token'];
 
 }
