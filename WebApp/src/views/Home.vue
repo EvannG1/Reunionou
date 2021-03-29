c05ff8c12bc7b1129d95742304fe9e4e7c63ee89
Evann Gehin <evann.gehin@ezcorp.io>
2021-03-29 17:53:41 +0200

    Add event map

--- WebApp/src/views/Home.vue
+++ WebApp/src/views/Home.vue
@@ -12,7 +12,22 @@
       <div class="row">
         <div class="col-7">
           <div class="card mb-3">
-            <img src="http://placehold.it/600x250" class="card-img-top" alt="">
+            <l-map v-if="showMap" :zoom="zoom" :center="center" :options="mapOptions" style="height: 350px; width: 100%;"
+              @update:center="centerUpdate" @update:zoom="zoomUpdate">
+              <l-tile-layer :url="url" :attribution="attribution" />
+              <l-marker :lat-lng="withTooltip">
+                <l-tooltip :options="{ permanent: true, interactive: true }">
+                  <div @click="innerClick">
+                    {{ location.name }}
+                    <p v-show="showParagraph">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                      sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.
+                      Donec finibus semper metus id malesuada.
+                    </p>
+                  </div>
+                </l-tooltip>
+              </l-marker>
+            </l-map>
             <div class="card-body">
               <h5 class="card-title">{{ title }}</h5>
               <p class="card-text">{{ description }}</p>
@@ -22,7 +37,7 @@
         </div>
         <div class="col-5">
           <div v-for="(event, index) in events" class="list-group">
-            <a @click="selectEvent(event.title, event.description, event.date, event.author, index)" href="#" class="list-group-item list-group-item-action">
+            <a @click="selectEvent(event.title, event.description, event.date, event.author, event.location, index)" href="#" class="list-group-item list-group-item-action">
               <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">{{ event.title }}</h5>
                 <small><font-awesome-icon icon="map-marker-alt"></font-awesome-icon> {{ event.location.name }}</small>
@@ -46,10 +61,25 @@
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LTooltip } from "vue2-leaflet";
+import { Icon } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    LMap,
+    LTileLayer,
+    LMarker,
+    LTooltip
   },
 
   data() {
@@ -59,16 +89,46 @@ export default {
       title: false,
       description: false,
       date: false,
-      author: false
+      author: false,
+      location: false,
+
+      zoom: 15,
+      center: latLng(location.x, location.y),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      withTooltip: latLng(47.41422, -1.250482),
+      currentZoom: 15,
+      currentCenter: latLng(location.x, location.y),
+      showParagraph: false,
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      showMap: true
     }
   },
   methods: {
-    selectEvent(title, description, date, author, index) {
+    selectEvent(title, description, date, author, location, index) {
       this.index = index;
       this.title = title;
       this.description = description;
       this.date = date;
       this.author = author.fullname;
+      this.location = location;
+      this.center = latLng(location.x, location.y)
+      this.currentCenter = latLng(location.x, location.y)
+      this.withTooltip = latLng(location.x, location.y)
+    },
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    showLongText() {
+      this.showParagraph = !this.showParagraph;
+    },
+    innerClick() {
+      alert("Click!");
     }
   },
   mounted() {
@@ -77,6 +137,10 @@ export default {
         this.description = response.data.owned[0].description;
         this.date = response.data.owned[0].date;
         this.author = response.data.owned[0].author.fullname;
+        this.location = response.data.owned[0].location;
+        this.center = latLng(response.data.owned[0].location.x, response.data.owned[0].location.y)
+      this.currentCenter = latLng(response.data.owned[0].location.x, response.data.owned[0].location.y)
+        this.withTooltip = latLng(response.data.owned[0].location.x, response.data.owned[0].location.y)
         response.data.owned.forEach(element => {
           this.events.push(element);
         });
