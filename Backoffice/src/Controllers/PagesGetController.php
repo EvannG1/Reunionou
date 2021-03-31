e571ac603d7f84cb2649f51c6d461c6ea0e2cb4b
Ludovic Caron <ludovic.c30@outlook.com>
2021-03-31 13:00:56 +0000

    ajout de la function qui affiche les events dans le controller PagesGetController.php

--- Backoffice/src/Controllers/PagesGetController.php
+++ Backoffice/src/Controllers/PagesGetController.php
@@ -3,6 +3,7 @@ namespace App\Controllers;
 
 use App\Controllers\Auth\AuthController;
 use App\Models\User;
+use App\Models\Event;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -32,6 +33,11 @@ class PagesGetController extends Controller {
     public function users(Request $request, Response $response) {
         $users = User::select()->get();
         $this->render($response, 'Pages/Users.twig', ['users' => $users]);
+    }
+
+    public function events(Request $request, Response $response) {
+        $events = Event::select()->get();
+        $this->render($response, 'Pages/Events.twig', ['events' => $events]);
     }
 
 }
